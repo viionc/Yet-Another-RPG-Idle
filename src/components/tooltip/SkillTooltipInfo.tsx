4f88f575a1d04d78107648cb1aca662c54090d8f
viionc <marek.d.stankiewicz@gmail.com>
2023-12-26 22:54:10 +0100

    style: enhanced tooltip for skills

--- src/components/tooltip/SkillTooltipInfo.tsx
+++ src/components/tooltip/SkillTooltipInfo.tsx
@@ -10,6 +10,15 @@ function SkillTooltipInfo({skill}: SkillToolTipInfoProps) {
         <>
             <div className="text-yellow-500">{name}</div>
             <div>{description}</div>
+            <div className="flex gap-2 mt-4 ms-auto">
+                <span className="flex gap-1  items-center text-xs">
+                    Add 1<img src="./other/leftClick.png" alt={`left click to add 1`} height={16} width={16}></img>
+                </span>
+                <span className="flex gap-1 ms-4 items-center text-xs">
+                    Add Max <span className="text-yellow-500">Ctrl +</span>
+                    <img src="./other/leftClick.png" alt={`left click to add 1`} height={16} width={16}></img>
+                </span>
+            </div>
         </>
     );
 }
